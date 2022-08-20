944d127574a45dcbfcdf4c5ca15c491b16164288
AzureAD\SteveNguyen <steve.nguyen@patientzero.com.au>
2022-08-20 22:59:06 +1000

    ability to add and show output for todolist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatListModule} from '@angular/material/list';
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +23,9 @@ import {MatButtonModule} from '@angular/material/button';
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main-form/main-form.component.ts
+++ src/app/main-form/main-form.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-main-form',
   templateUrl: './main-form.component.html',
   styleUrls: ['./main-form.component.css']
 })
-export class MainFormComponent implements OnInit {
+export class MainFormComponent implements OnInit,OnDestroy {
 
-  constructor() { }
+  itemList: string[] = []
+  todoFormGroup: FormGroup;
+  formBuilder: FormBuilder;
+  constructor(fb: FormBuilder) {
+    this.formBuilder = fb;
+    this.todoFormGroup = this.createForm()
+   }
 
   ngOnInit(): void {
+    
   }
+  ngOnDestroy(): void {
+      this.todoFormGroup.reset();
+  }
+  
 
+  createForm(): FormGroup {
+    return this.formBuilder.group(
+      {
+        todoItem: ['',Validators.required]
+      }
+    )
+  }
+  getValue(){
+    return this.todoFormGroup.value.todoItem;
+  }
+  createItem(){
+    this.itemList.push(this.getValue())
+    this.todoFormGroup.reset();
+  }
+}
+
+export interface TodoForm {
+  todoItem: FormControl;
+}
+export class TodoItem {
+
+  constructor(public anItem: string){
+
+  }
 }
